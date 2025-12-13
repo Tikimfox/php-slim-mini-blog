32fa49cbea1f7c57840e79169a5455a5bba261e0
Tikimfox <tikim.fox@gmail.com>
2025-12-13 09:34:28 +0100

    feat(config): add database configuration file for MySQL connection
    - create config.php for database settings
    - include driver, host, port, database, username, and password

---
+++ htdocs/app/Config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Database Configuration
+return [
+    'db' => [
+        'driver' => 'mysql',
+        'host' => 'mysql',
+        'port' => '3306',
+        'database' => 'miniblog',
+        'username' => 'root',
+        'password' => ''
+    ]
+];
